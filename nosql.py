5419414f1c9e1e09f3ddcab431f9a72b685088e1
notroot <notroot@ubuntu.(none)>
2012-05-16 18:47:58 -0400

    projekt nosql

--- nosql.py
+++ nosql.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 import urllib2
-import pymongo 
 import json
-import sys
 from pymongo import Connection
 from bson.code import Code
 from  couchdb import Server,PreconditionFailed
 
 mongoport=27017
-mongosrv="localhost"
+mongosrv="127.0.0.1"
 cdbport=5984
-cdbsrv="localhost"
+cdbsrv="127.0.0.1"
 
 url="http://blip.pl/statuses/all?limit=5" 
 data = urllib2.urlopen(url).read()
@@ -20,17 +18,17 @@ json = json.loads(data)
 dbm.statusy.insert(json)
 
 map = Code("function () {"
-"this.body.match(/[a-zA-Z\u0104-\u017c]+/g).forEach(function(z) {"
-"emit(z, 1);"
+"this.body.match(/[a-zA-Z\u0104-\u017c]+/g).forEach(function(x) {"
+"emit(x, 1);"
 "});"
 "}")
 
 reduce = Code("function (key, values) {"
-"var total = 0;"
+"var sum = 0;"
 "for (var i = 0; i < values.length; i++) {"
-"   total += values[i];"
+"   sum += values[i];"
 "}"
-"return total;"
+"return sum;"
 "}")
 
 result=dbm.statusy.map_reduce(map,reduce,"full_response=True")
@@ -42,18 +40,19 @@ for row in result.find():
 server = Server("http://"+cdbsrv+":"+str(cdbport))
 
 try:
-  dbc = server.create("blip")
+  dbc = server.create('blip')
 except PreconditionFailed:
-  del server["blip"]
-  dbc = server.create("blip")
-  dbc = server["blip"]
+  del server['blip']
+  dbc = server.create('blip')
+  dbc = server['blip']
   
 for row in dbm.statusy.find():
-  dbc.save({"body":row["body"]})
+  del row["_id"]
+  dbc.save(row)
   
 map = '''function(doc) {
-  doc.body.match(/[a-zA-Z\u0104-\u017c]+/g).forEach(function(z) {
-  emit(z,1); 
+  doc.body.match(/[a-zA-Z\u0104-\u017c]+/g).forEach(function(x) {
+  emit(x,1); 
 } )
 }'''
 reduce = '''function(key,values) {
--- readme.md
+++ readme.md
@@ -3,8 +3,15 @@ Kolejnym etapem działania skryptu jest przeniesienie tych danych do Couchdb i z
 Wynikiem działania MapReduce dla obydwóch baz jest statystyka takich samych wyrazów znajdujących się w statusach.
 
 Do poprawnego działania skryptu niezbędny jest interpreter języka Python w wersji 2.6-2.7 oraz najnowsze wersje modułów pymongo i couchdb.
-Parametry do połączenia z serwerem dla obydwóch systemów takie jak port i adres są ustawione  do testowania lokalnie i można je zmienić na początku kodu. 
+Wszystkie niezbędne moduły znajdują się w repozytorium, więc w przypadku ich braku
+nie trzeba ich doinstalowywać, wystarczy sklonować całe repozytorium.
+Skrypt uruchamia się poleceniem:
+
+'''
+python nosql.py
+'''
 Po nawiązaniu połączenia z serwerami automatycznie tworzone są odpowiednie bazy i kolekcje.
- 
+Parametry do połączenia z serwerem dla obydwóch systemów takie jak port i adres są ustawione  do testowania lokalnie i można je zmienić na początku kodu. 
+
 
 
